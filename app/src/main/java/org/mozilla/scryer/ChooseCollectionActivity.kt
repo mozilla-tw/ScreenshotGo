30decf230ecd20aaf4d4c25a8386338ab3847e18
Roger <pharspher@gmail.com>
2018-07-06 11:39:57 +0800

    Refine ChooseCollectionActivity

--- app/src/main/java/org/mozilla/scryer/ChooseCollectionActivity.kt
+++ app/src/main/java/org/mozilla/scryer/ChooseCollectionActivity.kt
@@ -6,6 +6,7 @@
 
 package org.mozilla.scryer
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Rect
 import android.os.Bundle
@@ -13,15 +14,24 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
 import android.view.*
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_choose_collection.*
-
 
 class ChooseCollectionActivity : AppCompatActivity() {
+    companion object {
+        private const val GRID_SPAN_COUNT = 2
+        private const val GRID_CELL_SPACE_DP = 6f
+    }
+
     private val collectionModel = CollectionModel()
+    private val adapter: CollectionAdapter by lazy {
+        CollectionAdapter(this, collectionModel.items, this::onItemClicked)
+    }
+
+    private val recyclerView: RecyclerView by lazy { findViewById<RecyclerView>(R.id.recycler_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +41,12 @@ class ChooseCollectionActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        recyclerView.layoutManager = GridLayoutManager(this, 2,
-                GridLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = GridLayoutManager(this, GRID_SPAN_COUNT,
+                GridLayoutManager.VERTICAL,
+                false)
+        recyclerView.addItemDecoration(SpacesItemDecoration(dp2px(this, GRID_CELL_SPACE_DP)))
 
-        recyclerView.adapter = ChooseCollectionAdapter(this, collectionModel.items, this::onItemClicked)
-        recyclerView.addItemDecoration(SpacesItemDecoration(20))
+        recyclerView.adapter = this.adapter
     }
 
     private fun onItemClicked(item: CollectionItem) {
@@ -44,17 +55,18 @@ class ChooseCollectionActivity : AppCompatActivity() {
     }
 
     fun onNewCollectionClicked() {
-        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_add_collection, null, false)
+        val dialogView = View.inflate(this, R.layout.dialog_add_collection, null)
+        val editText = dialogView.findViewById<EditText>(R.id.edit_text)
 
         val dialog = AlertDialog.Builder(this, R.style.Theme_AppCompat_Light_Dialog_Alert)
                 .setView(dialogView)
                 .setPositiveButton("DONE", { _, _ ->
-                    collectionModel.addNewCollection(CollectionItem(dialogView.findViewById<EditText>(R.id.edit_text)?.text.toString()))
+                    collectionModel.addNewCollection(CollectionItem(editText.text.toString()))
                     recyclerView.adapter.notifyItemInserted(recyclerView.adapter.itemCount - 1)
                 })
                 .create()
         dialog.show()
-        dialogView.findViewById<EditText>(R.id.edit_text).requestFocus()
+        editText.requestFocus()
         dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
     }
 }
@@ -64,57 +76,108 @@ class CollectionModel {
         items.add(item)
     }
 
-    val items: MutableList<CollectionItem> = mutableListOf(CollectionItem("Create new collection"),
-            CollectionItem("Shopping"),
+    val items: MutableList<CollectionItem> = mutableListOf(CollectionItem("Shopping"),
             CollectionItem("Music"),
             CollectionItem("Secret"))
 }
 
 class CollectionItem(val title: String)
 
-class ChooseCollectionAdapter(private var activity: ChooseCollectionActivity,
-                              private var list: MutableList<CollectionItem>,
-                              private var itemClickListener: (CollectionItem) -> Unit)
+class CollectionAdapter(private var activity: ChooseCollectionActivity,
+                        private var list: MutableList<CollectionItem>,
+                        private var itemClickListener: (CollectionItem) -> Unit)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        private const val TYPE_FIXED_ITEM = 0
+        private const val TYPE_DATA_ITEM = 1
+
+        private const val POSITION_NEW_COLLECTION = 0
+    }
+
+    private val fixedItems = listOf("Create new collection")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            TYPE_FIXED_ITEM -> {
+                return createFixedViewHolder(parent)
+            }
+
+            TYPE_DATA_ITEM -> {
+                return createDataViewHolder(parent)
+            }
+        }
+        throw IllegalStateException("unexpected item type $viewType")
+    }
+
+    override fun getItemCount(): Int {
+        return fixedItems.size + list.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < fixedItems.size) TYPE_FIXED_ITEM else TYPE_DATA_ITEM
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is DataViewHolder) {
+            (holder.itemView as TextView).text = list[position - fixedItems.size].title
+        } else if (holder is FixedViewHolder) {
+            (holder.itemView as TextView).text = fixedItems[position]
+        }
+    }
+
+    private fun createFixedViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
         val view = TextView(parent.context)
         view.setBackgroundColor(Color.LTGRAY)
         view.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 500)
         view.setTextColor(Color.BLACK)
         view.gravity = Gravity.CENTER
 
-        val holder = ItemHolder(view)
-        holder.itemView.setOnClickListener {
-            holder.adapterPosition.takeIf { it != RecyclerView.NO_POSITION }?.run {
-                if (this == 0) {
-                    createNewCollection()
-                } else {
-                    itemClickListener.invoke(list[this])
-                }
+        val holder = FixedViewHolder(view)
+        holder.itemView.setOnClickListener { _ ->
+            holder.adapterPosition.takeIf { position ->
+                position != RecyclerView.NO_POSITION
+
+            }?.let { position: Int ->
+                onFixedItemClicked(position)
             }
         }
         return holder
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
+    private fun createDataViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        val view = TextView(parent.context)
+        view.setBackgroundColor(Color.LTGRAY)
+        view.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 500)
+        view.setTextColor(Color.BLACK)
+        view.gravity = Gravity.CENTER
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is ItemHolder) {
-            (holder.itemView as TextView).text = list[position].title
+        val holder = DataViewHolder(view)
+        holder.itemView.setOnClickListener { _ ->
+            holder.adapterPosition.takeIf { position ->
+                position != RecyclerView.NO_POSITION
+
+            }?.let { position: Int ->
+                itemClickListener.invoke(list[position - fixedItems.size])
+            }
         }
+        return holder
     }
 
     private fun createNewCollection() {
         activity.onNewCollectionClicked()
     }
+
+    private fun onFixedItemClicked(position :Int) {
+        when (position) {
+            POSITION_NEW_COLLECTION -> {
+                createNewCollection()
+            }
+        }
+    }
 }
 
-class ItemHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-}
+class DataViewHolder(view: View) : RecyclerView.ViewHolder(view)
+class FixedViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
 class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
 
@@ -136,4 +199,8 @@ class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration
             outRect.top = 0
         }
     }
+}
+
+private fun dp2px(context: Context, dp: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics).toInt()
 }

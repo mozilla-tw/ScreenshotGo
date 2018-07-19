33058bb7e962fcbe21183daa35990da16b8b1799
Benjamin Cheng <chengwc@gmail.com>
2018-07-19 14:25:09 +0800

    Integrate ML kit for text recognition

--- app/src/main/java/org/mozilla/scryer/detailpage/DetailPageActivity.kt
+++ app/src/main/java/org/mozilla/scryer/detailpage/DetailPageActivity.kt
@@ -8,16 +8,23 @@ package org.mozilla.scryer.detailpage
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.view.ViewCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import org.mozilla.scryer.R
 
 class DetailPageActivity : AppCompatActivity() {
+
+    private val mGraphicOverlay: GraphicOverlay by lazy { findViewById<GraphicOverlay>(R.id.graphic_overlay) }
 
     companion object Launcher {
         fun showDetailPage(context: Context, path: String, srcView: View?) {
@@ -38,6 +45,43 @@ class DetailPageActivity : AppCompatActivity() {
 
         val imageView = findViewById<ImageView>(R.id.image_view)
         val path = intent.getStringExtra("path")
-        imageView.setImageBitmap(BitmapFactory.decodeFile(path))
+        val bitmap = BitmapFactory.decodeFile(path)
+        bitmap?.let {
+            imageView.setImageBitmap(it)
+            runTextRecognition(it)
+        }
+    }
+
+    private fun runTextRecognition(selectedImage: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(selectedImage)
+        val detector = FirebaseVision.getInstance().visionTextDetector
+        detector.detectInImage(image)
+                .addOnSuccessListener { texts ->
+                    processTextRecognitionResult(texts)
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    e.printStackTrace()
+                }
+    }
+
+    private fun processTextRecognitionResult(texts: FirebaseVisionText) {
+        val blocks = texts.blocks
+        if (blocks.size == 0) {
+            Toast.makeText(applicationContext, "No text found", Toast.LENGTH_SHORT).show()
+            return
+        }
+        mGraphicOverlay.clear()
+        for (i in blocks.indices) {
+            val lines = blocks[i].lines
+            for (j in lines.indices) {
+                val elements = lines[j].elements
+                for (k in elements.indices) {
+                    val textGraphic = TextGraphic(mGraphicOverlay, elements[k])
+                    mGraphicOverlay.add(textGraphic)
+
+                }
+            }
+        }
     }
 }

dbe1bd5f51814e7a40a455e6f8862fee4cfcfdb5
Roger <pharspher@gmail.com>
2018-08-09 17:06:46 +0800

    Implement collection name dialog UI.

--- app/src/main/java/org/mozilla/scryer/capture/SortingPanelActivity.kt
+++ app/src/main/java/org/mozilla/scryer/capture/SortingPanelActivity.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
@@ -16,6 +17,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import org.mozilla.scryer.R
 import org.mozilla.scryer.persistence.CollectionModel
@@ -195,18 +197,26 @@ class SortingPanelActivity : AppCompatActivity() {
     }
 
     private fun onNewCollectionClicked() {
-        val dialogView = View.inflate(this, R.layout.dialog_add_collection, null)
+        val dialogView = View.inflate(this, R.layout.dialog_collection_name, null)
         val editText = dialogView.findViewById<EditText>(R.id.edit_text)
 
         val random = Random()
         val dialog = AlertDialog.Builder(this, R.style.Theme_AppCompat_Light_Dialog_Alert)
-                .setView(dialogView)
-                .setPositiveButton("DONE") { _, _ ->
+                .setPositiveButton(R.string.ac_done) { _, _ ->
                     val model = CollectionModel(editText.text.toString(), System.currentTimeMillis(), Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255)))
                     screenshotViewModel.addCollection(model)
-                }.create()
+                }
+                .setNegativeButton(R.string.ac_cancel) { _, _ ->
+                }
+                .setView(dialogView)
+                .create()
 
-        dialog.setOnShowListener { dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false }
+        dialog.setOnShowListener {
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+            val colors = ContextCompat.getColorStateList(this, R.color.primary_text_button)
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(colors)
+            dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(colors)
+        }
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -216,6 +226,11 @@ class SortingPanelActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable?) {}
         })
+
+        val title = dialogView.findViewById<TextView>(R.id.title)
+        title.text = resources.getText(R.string.collection_dialog_title_new_collection)
+
+        dialog.window.setBackgroundDrawable(null)
 
         dialog.show()
         editText.requestFocus()

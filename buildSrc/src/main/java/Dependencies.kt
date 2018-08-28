25c647a1bed75349ee6a44fe9a129a81c20063fe
Roger <pharspher@gmail.com>
2018-08-28 11:40:59 +0800

    Use kotlin-dsl for dependency management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,23 @@
+object Versions {
+    // Android
+    val support = "27.1.1"
+    val constraint_layout = "1.1.2"
+    val arch_lifecycle = "1.1.1"
+    val arch_room = "1.1.1"
+    val arch_navigation = "1.0.0-alpha05"
+
+    // Google
+    val firebase = "16.0.1"
+    val firebase_ml = "16.0.0"
+
+    val kotlin = "1.2.51"
+
+    // Test
+    val junit = "4.12"
+    val test_runner = "1.0.2"
+    val espresso = "3.0.2"
+    val mockito = "2.12.0"
+
+    // 3rd party
+    val glide = "4.7.1"
+}

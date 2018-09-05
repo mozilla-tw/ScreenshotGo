cc6ed925854979c49ab836b11503042edecfdd07
Roger <pharspher@gmail.com>
2018-09-05 11:21:51 +0800

    [DetailPage] Implement action bar & loading view

--- app/src/main/java/org/mozilla/scryer/detailpage/DetailPageActivity.kt
+++ app/src/main/java/org/mozilla/scryer/detailpage/DetailPageActivity.kt
@@ -12,10 +12,14 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.constraint.Group
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.View
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
@@ -32,6 +36,7 @@ import java.io.File
 class DetailPageActivity : AppCompatActivity() {
 
     private val mGraphicOverlay: GraphicOverlay by lazy { findViewById<GraphicOverlay>(R.id.graphic_overlay) }
+    private val toolbarGroup: Group by lazy { findViewById<Group>(R.id.toolbar_group) }
 
     companion object Launcher {
         fun showDetailPage(context: Context, path: String, srcView: View?) {
@@ -50,7 +55,13 @@ class DetailPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
 
+        initActionBar()
+
         val imageView = findViewById<ImageView>(R.id.image_view)
+        imageView.setOnClickListener {
+            toggleActionBar()
+        }
+
         val path = intent.getStringExtra("path")
         val bitmap = BitmapFactory.decodeFile(path)
         bitmap?.let {
@@ -69,6 +80,39 @@ class DetailPageActivity : AppCompatActivity() {
                 return false
             }
         }).into(imageView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initActionBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun toggleActionBar() {
+        var isShowing = supportActionBar?.isShowing ?: false
+        isShowing = !isShowing
+
+        if (isShowing) {
+            toolbarGroup.visibility = View.VISIBLE
+            supportActionBar?.show()
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        } else {
+            toolbarGroup.visibility = View.INVISIBLE
+            supportActionBar?.hide()
+            window?.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
     }
 
     private fun runTextRecognition(selectedImage: Bitmap) {

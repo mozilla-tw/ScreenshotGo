1625169c8ed2d589a9777d58cf3311e892dd0f16
Roger <pharspher@gmail.com>
2018-09-06 17:50:20 +0800

    [DetailPage] Improve debug text graphic

--- app/src/main/java/org/mozilla/scryer/detailpage/TextGraphic.kt
+++ app/src/main/java/org/mozilla/scryer/detailpage/TextGraphic.kt
@@ -1,9 +1,6 @@
 package org.mozilla.scryer.detailpage
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.Log
 
 import com.google.firebase.ml.vision.text.FirebaseVisionText
@@ -14,11 +11,11 @@ class TextGraphic internal constructor(overlay: GraphicOverlay, private val bloc
     private val textPaint: Paint = Paint()
 
     init {
-        rectPaint.color = TEXT_COLOR
-        rectPaint.style = Paint.Style.STROKE
+        rectPaint.color = Color.parseColor("#ccffcc00")
+        rectPaint.style = Paint.Style.FILL_AND_STROKE
         rectPaint.strokeWidth = STROKE_WIDTH
 
-        textPaint.color = TEXT_COLOR
+        textPaint.color = Color.BLACK
         textPaint.textSize = TEXT_SIZE
         // Redraw the overlay, as this graphic has been added.
         postInvalidate()
@@ -33,15 +30,41 @@ class TextGraphic internal constructor(overlay: GraphicOverlay, private val bloc
             throw IllegalStateException("Attempting to draw a null text.")
         }
 
-        for (line in block.lines.toMutableList().apply{ sortBy{ it.boundingBox?.centerY() } }) {
-            line.boundingBox?.let {
-                canvas.drawText(line.text, it.left.toFloat(), it.bottom.toFloat(), textPaint)
-            }
-        }
-
         // Draws the bounding box around the TextBlock.
         val rect = RectF(block.boundingBox)
         canvas.drawRect(rect, rectPaint)
+
+        for (line in block.lines.toMutableList().apply{ sortBy{ it.boundingBox?.centerY() } }) {
+            line.boundingBox?.let {
+                estimateSize(line.text, it)
+                canvas.drawText(line.text, it.left.toFloat(), it.bottom.toFloat() /*- textPaint.fontMetrics.bottom*/, textPaint)
+            }
+        }
+    }
+
+    private fun estimateSize(text: String, boundingBox: Rect) {
+        val width = boundingBox.width()
+
+        var start = 1
+        var end = 300
+        val bound = Rect()
+        while (start < end) {
+            val size = start + (end - start) / 2
+            textPaint.textSize = size.toFloat()
+            textPaint.getTextBounds(text, 0, text.length, bound)
+
+            val measureWidth = bound.right - bound.left
+            val diffWidth = width - measureWidth
+            if (diffWidth == 0) {
+                return
+            } else {
+                if (diffWidth > 0) {
+                    start = size + 1
+                } else {
+                    end = size - 1
+                }
+            }
+        }
     }
 
     companion object {

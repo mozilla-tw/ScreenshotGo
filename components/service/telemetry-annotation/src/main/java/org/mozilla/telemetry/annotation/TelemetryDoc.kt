mozilla-tw/Project-Scry

Generate Telemetry doc automatically

components/service/telemetry-annotation/src/main/java/org/mozilla/telemetry/annotation/TelemetryDoc.kt
@@ -1,0 +1,18 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.telemetry.annotation
+
+import kotlin.annotation.Retention
+
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class TelemetryDoc(
+        val name: String,
+        val value: String,
+        val action: String,
+        val method: String,
+        val `object`: String,
+        val extras: Array<TelemetryExtra>)

mozilla-tw/ScreenshotGo

components/service/telemetry-annotation/src/main/java/org/mozilla/telemetry/annotation/TelemetryDoc.kt
@@ -1,0 +1,18 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.telemetry.annotation
+
+import kotlin.annotation.Retention
+
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class TelemetryDoc(
+        val name: String,
+        val value: String,
+        val category: String,
+        val method: String,
+        val `object`: String,
+        val extras: Array<TelemetryExtra>)
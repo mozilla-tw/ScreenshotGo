0c3778c14830fa5267e9081d0602d1ff3a59e54f
Roger <pharspher@gmail.com>
2018-11-29 10:59:05 +0800

    [ScreenshotFetcher] Add error handling to avoid exceptions reported on Crashlytic

--- app/src/main/java/org/mozilla/scryer/filemonitor/ScreenshotFetcher.kt
+++ app/src/main/java/org/mozilla/scryer/filemonitor/ScreenshotFetcher.kt
@@ -25,27 +25,46 @@ class ScreenshotFetcher {
         val selection = "${MediaStore.Images.ImageColumns.BUCKET_ID} IS NOT NULL) GROUP BY (${MediaStore.Images.ImageColumns.BUCKET_ID}"
         val results = mutableListOf<String>()
 
-        context.contentResolver.query(uri,
-                columns,
-                selection,
-                null,
-                null
-        ).use {
-            val cursor = it ?: return@use
-            while (cursor.moveToNext()) {
-                val index = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
-                if (index < 0) {
-                    continue
-                }
-                val path = cursor.getString(index).trim()
-                if (path.contains("screenshot", true)) {
-                    val folder = File(path).parent?.trimEnd(File.separatorChar) ?: continue
-                    results.add(folder)
+        try {
+            context.contentResolver.query(uri,
+                    columns,
+                    selection,
+                    null,
+                    null
+            ).use {
+                val cursor = it ?: return@use
+                while (cursor.moveToNext()) {
+                    val index = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                    if (index < 0) {
+                        continue
+                    }
+
+                    // whether getString() throws exception is not defined and depends on implementation,
+                    // so here simply catch the exception and ignore fail cases
+                    val path = getCursorStringOrEmpty(cursor, index)
+                    if (path.contains("screenshot", true)) {
+                        val folder = File(path).parent?.trimEnd(File.separatorChar) ?: continue
+                        results.add(folder)
+                    }
                 }
             }
+        } catch (e: SecurityException) {
+            // TODO: It's unclear what king of permission is needed from the log on Crashlytics,
+            // maybe there're some others permissions that are needed on some devices (e.g. Redmi 5 Plus)
+            // Permission Denial: reading com.android.providers.media.MediaProvider uri
+            // content://media/external/images/media from pid=4453, uid=1410264 requires null,
+            // or grantUriPermission()
         }
 
         return results
+    }
+
+    private fun getCursorStringOrEmpty(cursor: Cursor, idx: Int): String {
+        return try {
+            cursor.getString(idx).trim()
+        } catch (e: Exception) {
+            ""
+        }
     }
 
     private fun fetchScreenshots(dirPath: String): List<ScreenshotModel> {

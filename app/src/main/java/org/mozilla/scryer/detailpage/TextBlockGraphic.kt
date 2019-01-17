d736e7c3a5c7835e1f33cd039195ea57a5c692ec
Roger <pharspher@gmail.com>
2019-01-17 13:20:49 +0800

    [DetailPage] Adjust highlight color

--- app/src/main/java/org/mozilla/scryer/detailpage/TextBlockGraphic.kt
+++ app/src/main/java/org/mozilla/scryer/detailpage/TextBlockGraphic.kt
@@ -5,22 +5,37 @@
 package org.mozilla.scryer.detailpage
 
 import android.graphics.*
+import androidx.core.content.ContextCompat
 import com.google.firebase.ml.vision.text.FirebaseVisionText
 import org.mozilla.scryer.BuildConfig
+import org.mozilla.scryer.R
 import org.mozilla.scryer.extension.dpToPx
 
 class TextBlockGraphic internal constructor(
-        overlay: GraphicOverlay,
+        val overlay: GraphicOverlay,
         val block: FirebaseVisionText.TextBlock
 ) : GraphicOverlay.Graphic(overlay) {
 
     companion object {
-        private val COLOR_NORMAL = Color.parseColor("#52000000")
+        private const val colorNormalRes = android.R.color.black
+        private const val alphaNormal = 0.32f
+
+        private const val colorSelectedRes = R.color.primaryTeal
+        private const val alphaSelected = 0.3f
+
         private const val CORNER_RADIUS_DP = 2f
     }
 
     private val cornerRadius = CORNER_RADIUS_DP.dpToPx(overlay.context.resources.displayMetrics).toFloat()
     val boundingBox = RectF()
+
+    private val normalColor: Int = ContextCompat.getColor(overlay.context, colorNormalRes).let {
+        Color.argb((255 * alphaNormal).toInt(), Color.red(it), Color.green(it), Color.blue(it))
+    }
+
+    private val selectedColor: Int = ContextCompat.getColor(overlay.context, colorSelectedRes).let {
+        Color.argb((255 * alphaSelected).toInt(), Color.red(it), Color.green(it), Color.blue(it))
+    }
 
     private val debugPaint: Paint? = if (BuildConfig.DEBUG) {
         Paint().apply {
@@ -45,25 +60,23 @@ class TextBlockGraphic internal constructor(
 
     init {
         rectPaint.apply {
-            color = COLOR_NORMAL
             style = Paint.Style.FILL
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
         }
         postInvalidate()
     }
 
     override fun draw(canvas: Canvas) {
         boundingBox.set(block.boundingBox)
-        rectPaint.xfermode = if (isSelected) {
-            PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+
+        rectPaint.color = if (isSelected) {
+            selectedColor
         } else {
-            PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+            normalColor
         }
 
-        val tX = +translationX
-        val tY = +translationY
-
-        val left = boundingBox.left * scale + tX
-        val top = boundingBox.top * scale + tY
+        val left = boundingBox.left * scale + translationX
+        val top = boundingBox.top * scale + translationY
         val width = (boundingBox.right - boundingBox.left) * scale
         val height = (boundingBox.bottom - boundingBox.top) * scale
 
